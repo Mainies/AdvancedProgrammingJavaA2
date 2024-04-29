b779c66621e2c47dd9f8009ee0b807b19dabba26
unknown <sammainwood@homtail.com>
2024-04-29 12:06:37 +1000

    Change some structure for ease in early development. First UI aspect created, Order using OrderPane Controller and FXML

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,28 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = (Parent) FXMLLoader.load(getClass().getResource("Orderer.fxml"));
+			Scene scene = new Scene(root);
+			//String css = this.getClass().getResource("application.css").toExternalForm();
+			//scene.getStylesheets().add(css);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ src/application/OrderPaneController.java
@@ -0,0 +1,96 @@
+package application;
+
+import javafx.event.ActionEvent;
+import restaurant.Order;
+import restaurant.PointOfService;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import exceptions.*;
+
+
+public class OrderPaneController {
+	@FXML
+	private TextField burrito;
+	
+	@FXML
+	private TextField fries;
+	
+	@FXML
+	private TextField sodas;
+	
+	@FXML
+	private TextField meals;
+	
+	@FXML
+	private Label errorMessage;
+	
+	@FXML
+	private Button make_order;
+	
+	public Order orderFood(ActionEvent event) throws Exception {
+		try{
+			validateOrderInput();
+		} catch (Exception e) {
+			errorMessage.setText("Error: " + e.getMessage());
+		}	
+		
+		int numBurritos = Integer.parseInt(burrito.getText());
+		int numFries = Integer.parseInt(fries.getText());
+		int numSodas = Integer.parseInt(sodas.getText());
+		int numMeals = Integer.parseInt(meals.getText());
+		
+		numBurritos += numMeals;
+		numFries += numMeals;
+		numSodas += numMeals;
+		Order order = new Order(numBurritos, numFries, numSodas, numMeals);
+		order.printOrder();
+		return order;
+	}
+	
+	public void validateOrderInput() throws Exception{
+		PointOfService pos = new PointOfService();
+		try{
+			pos.validateNumber(burrito.getText());
+			pos.validateNumber(fries.getText());
+			pos.validateNumber(sodas.getText());
+			pos.validateNumber(meals.getText());
+			}
+		catch (NotANumberException e) {
+				throw e;
+			}
+		try {
+			pos.wholeNumber(Double.parseDouble(burrito.getText()));
+	        pos.wholeNumber(Double.parseDouble(fries.getText()));
+	        pos.wholeNumber(Double.parseDouble(sodas.getText()));
+	        pos.wholeNumber(Double.parseDouble(meals.getText()));	
+		} 
+		catch(NotWholeNumber e) {
+			throw e;
+		}	
+		try {
+			pos.negativeInput(Integer.parseInt(burrito.getText()));
+			pos.negativeInput(Integer.parseInt(fries.getText()));
+			pos.negativeInput(Integer.parseInt(sodas.getText()));
+			pos.negativeInput(Integer.parseInt(meals.getText()));
+		}
+		catch(InvalidNegativeNumber e) {
+			throw e;
+		}
+	}
+	
+	@FXML
+	public void initializeListeners() {
+        burrito.textProperty().addListener((observable) -> {});
+        fries.textProperty().addListener((observable) -> {});
+        sodas.textProperty().addListener((observable) -> {});
+        meals.textProperty().addListener((observable) -> {});
+	}
+}
